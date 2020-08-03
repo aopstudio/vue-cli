8142a3a0a44681b4c4a260a20d315a7a4080a93c
Cheng Yi <cenzhedong2@126.com>
2020-08-03 20:51:16 +0800

    加入上传图片功能

--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -1,7 +1,7 @@
 <template>
   <el-main>
       <el-input type="text" v-model="article.title"></el-input>
-      <mavon-editor v-model="article.content" :toolbarsFlag="false" defaultOpen="preview" style="margin-top:15px"/>
+      <mavon-editor v-model="article.content" ref="md" :toolbarsFlag="true" @imgAdd="$imgAdd" @imgDel="$imgDel" defaultOpen="preview" style="margin-top:15px"/>
       <el-button type="primary" @click="submit()">提交</el-button>
   </el-main>
 </template>
@@ -32,6 +32,27 @@ export default {
                 me.$router.push('/');
             })
             .catch(function (error) {
+                console.log(error);
+            });
+        },
+        $imgAdd(pos, $file){
+            // 第一步.将图片上传到服务器.
+           var formdata = new FormData();
+           formdata.append('image', $file);
+           let me =this;
+           axios.post('http://localhost:8080/image',formdata, {
+             headers: { 'Content-Type': 'multipart/form-data' },
+           }
+           ).then(function (response){
+               // 第二步.将返回的url替换到文本原位置![...](0) -> ![...](url)
+               /**
+               * $vm 指为mavonEditor实例，可以通过如下两种方式获取
+               * 1. 通过引入对象获取: `import {mavonEditor} from ...` 等方式引入后，`$vm`为`mavonEditor`
+               * 2. 通过$refs获取: html声明ref : `<mavon-editor ref=md ></mavon-editor>，`$vm`为 `this.$refs.md`
+               */
+               me.$refs.md.$img2Url(pos, response.data);
+           })
+           .catch(function (error) {
                 console.log(error);
             });
         }
